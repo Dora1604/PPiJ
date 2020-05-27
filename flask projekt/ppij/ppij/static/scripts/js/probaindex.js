6e1f83e2c03a1ba5700225b4cfe463a6892f1a89
ancipet <as50674@fer.hr>
2020-05-27 11:54:08 +0200

    Toggle izmedu opcije 1 i opcije 2 popravljen

--- flask projekt/ppij/ppij/static/scripts/js/probaindex.js
+++ flask projekt/ppij/ppij/static/scripts/js/probaindex.js
@@ -1,9 +1,11 @@
 function showOption1() {
     document.getElementById('option1').style.display = "block";
+    document.getElementById('option2').style.display = "none";
 }
 
 function showOption2() {
     document.getElementById('option2').style.display = "block";
+    document.getElementById('option1').style.display = "none";
 }
 
 $('#datetimepicker1').datetimepicker({
