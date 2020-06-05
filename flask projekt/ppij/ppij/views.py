5f1f231583c1445e8393ad67f94f8e0d45674544
danicavladic <danica.vladic@fer.hr>
2020-06-05 18:50:51 +0200

    fico je jazavac

--- flask projekt/ppij/ppij/views.py
+++ flask projekt/ppij/ppij/views.py
@@ -7,24 +7,33 @@ from flask import render_template
 from ppij import app
 from flask import Flask, render_template, request, redirect, Response
 import random, json 
+from datetime import datetime
 
 @app.route('/')
 def home():
-    """Renders the home page."""
-    return render_template(
+     return render_template(
         'probaindex.html',
-        title='Home Page',
-        year=datetime.now().year,
-        #name = tony.istina()
     )
 
+@app.route('/probaindex', methods=('GET', 'POST'))
+def probaindex():
+     datum = request.form['datum']
+     datumi = datum.split('-')
+     datumi[0] = datumi[0].strip()
+     datumi[1] = datumi[1].strip()
+     durationBetweenDates = find_duration(datumi[0], datumi[1])
+     fourthOfJune = "04.06.2020"
+     dat1ToFourthOfJune = find_duration(datumi[0], fourthOfJune)
+     map = {'zagreb': 11.4, 'osijek': 11.4, 'pula': 11.4, 'split': 11.4, 'dubrovnik': 11.4, 'zadar': 11.4, 'rijeka': 11.4, 'sibenik': 11.4, 'varazdin': 11.4}
+     return render_template(
+        'probaindex.html',
+    )
 
-@app.route('/handle_data', methods=('GET', 'POST'))
-def handle_data():
-    tony3 = request.form['tony']
-    tonyNovi = tony3 + "je lijep"
-    tony4 = analiziraj(tonyNovi)
-    return tony4
-def analiziraj(tonyNovi):
-    t = tonyNovi+"da"
-    return t
+def find_duration(dat1, dat2):
+    date1 = datetime.strptime(dat1, '%d.%m.%Y').date()
+    date2 = datetime.strptime(dat2, '%d.%m.%Y').date()
+    return abs((date2 - date1).days)+1
+
+
+
+
