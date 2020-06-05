ac7ccaf4336f643fe4280f40527353f29e060807
ancipet <as50674@fer.hr>
2020-06-05 17:46:59 +0200

    Obrisani fileovi

--- flask projekt/ppij/ppij/views.py
+++ flask projekt/ppij/ppij/views.py
@@ -9,7 +9,6 @@ from flask import Flask, render_template, request, redirect, Response
 import random, json 
 
 @app.route('/')
-@app.route('/home')
 def home():
     """Renders the home page."""
     return render_template(
@@ -19,77 +18,6 @@ def home():
         #name = tony.istina()
     )
 
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
-
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
-
-@app.route('/proba')
-def proba():
-    """Renders the contact page."""
-    return render_template(
-        'proba.html',
-        title='Proba',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
-
-@app.route('/novi')
-def novi():
-    """Renders the contact page."""
-    return render_template(
-        'noviindex.html',
-        title='Index',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
-
-
-@app.route('/opcija1')
-def opcija1():
-    """Renders the contact page."""
-    return render_template(
-        'opcija1.html',
-        title='Index',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
-
-
-@app.route('/opcija2')
-def opcija2():
-    """Renders the contact page."""
-    return render_template(
-        'opcija2.html',
-        title='Index',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
-
-@app.route('/probaindex')
-def probaindex():
-    """Renders the contact page."""
-    return render_template(
-        'probaindex.html',
-        title='Index',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
 
 @app.route('/handle_data', methods=('GET', 'POST'))
 def handle_data():
